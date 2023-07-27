2f9cb2ccf4c7b5beccfae0509b5ac3d7b789c08c
Vimaser <thfeske@gmail.com>
2023-07-26 20:34:14 -0500

    routine_activities

--- db/routine_activities.js
+++ db/routine_activities.js
@@ -7,7 +7,8 @@ async function addActivityToRoutine({
   duration,
 }) {
   try {
-    const { rows } = await client.query(`
+    const { rows } = await client.query(
+      `
       INSERT INTO routine_activities("routineId", "activityId", count, duration)
       VALUES ($1, $2, $3, $4)
       RETURNING *;
@@ -25,11 +26,68 @@ async function addActivityToRoutine({
   }
 }
 
-async function getRoutineActivityById(id) {}
+async function getRoutineActivityById(id) {
+  try {
+    const {
+      rows: [routineActivity],
+    } = await client.query(
+      `
+  SELECT *
+  FROM routine_activities
+  WHERE id=$1
+  `,
+      [id]
+    );
+    if (!routineActivity) {
+      throw error;
+    }
+    return routineActivity;
+  } catch (error) {
+    throw error;
+  }
+}
 
-async function getRoutineActivitiesByRoutine({ id }) {}
+async function getRoutineActivitiesByRoutine({ id }) {
+  try {
+    const { rows } = await client.query(
+      `
+    SELECT *
+    FROM routine_activities
+    WHERE "routineId"=$1;
+    `,
+      [id]
+    );
 
-async function updateRoutineActivity({ id, ...fields }) {}
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function updateRoutineActivity({ id, ...fields }) {
+  try {
+  const updateFields = Object.keys(fields)
+    .map((key, index) => `"${key}" = $${index + 1}`)
+    .join(", ");
+
+  const values = Object.values(fields);
+
+  const {rows} = await client.query(
+    `
+    UPDATE routine_activies
+    SET ${updateFields}
+    WHERE id=$${values.length + 1}
+    RETURNING *;
+    `,
+    [...values, id]
+  );
+
+  const updatedRoutinesActivity = rows[0];
+
+  return updatedRoutinesActivity;
+} catch (error) {
+  throw error;
+}}
 
 async function destroyRoutineActivity(id) {}
 
