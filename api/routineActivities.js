f394167faee95c50a5916f834a72ebed491b0613
Vimaser <thfeske@gmail.com>
2023-07-31 17:39:32 -0500

    api work

--- api/routineActivities.js
+++ api/routineActivities.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const { 
+    getRoutineActivityById, 
+    addActivityToRoutine, 
+    getRoutineActivitiesByRoutine, 
+    updateRoutineActivity, 
+    destroyRoutineActivity, 
+    canEditRoutineActivity } = require('../db/routine_activities');
 // PATCH /api/routine_activities/:routineActivityId
 
 // DELETE /api/routine_activities/:routineActivityId
--- api/users.js
+++ api/users.js
@@ -2,8 +2,30 @@
 const express = require("express");
 const router = express.Router();
 
+const {   
+    createUser,
+    getUser,
+    getUserById,
+    getUserByUsername } = require('../db/users');
 // POST /api/users/register
 
+apiRouter.post('/api/users/register', async (req, res, next) => {
+    const { username, password, name, location } = req.body;
+    
+    try {
+        const _user = await getUserByUsername(username);
+
+        if (_user) {
+            next({
+                name: "UserExistsError",
+                message: "A user by that name already exists",
+            });
+        }
+        const user = await c
+    } catch (error) {
+        
+    }
+})
 // POST /api/users/login
 
 // GET /api/users/me
